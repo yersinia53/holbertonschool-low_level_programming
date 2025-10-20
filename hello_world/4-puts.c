608501ae4284d6406c301a4bb8092b0131259284
yersinia53 <jim.rapenne@gmail.com>
2025-10-20 13:11:47 +0000

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    char* str1 = "Programming is like building a multilingual puzzle";
+    char* str1 = "\"Programming is like building a multilingual puzzle";
     puts(str1);
 
     return 0;
