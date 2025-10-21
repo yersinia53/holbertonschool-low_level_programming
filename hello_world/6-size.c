c1bb0cc2f4abf0188713e10b185f71aac05e9e0e
yersinia53 <jim.rapenne@gmail.com>
2025-10-21 07:51:59 +0000

    6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,11 +11,11 @@ int main()
 	char c;
 	long int l;
 	long long int ll;
-	printf("Size of char: %d bytes\n", (int)sizeof(c));
-	printf("Size of int: %d bytes\n", (int)sizeof(i));
-	printf("Size of long int: %d bytes\n", (int)sizeof(l));
-	printf("Size of long long int: %d bytes\n", (int)sizeof(ll));
-	printf("Size of float: %d bytes\n", (int)sizeof(f));
+	printf("Size a of char: %d byte(s)\n", (int)sizeof(c));
+	printf("Size of a int: %d byte(s)\n", (int)sizeof(i));
+	printf("Size of a long int: %d byte(s)\n", (int)sizeof(l));
+	printf("Size of a long long int: %d byte(s)\n", (int)sizeof(ll));
+	printf("Size of a float: %d byte(s)\n", (int)sizeof(f));
 
 	return 0;
 }
