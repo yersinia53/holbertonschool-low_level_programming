7399d6104c7f1cf68f6075d8737f910222ecccec
yersinia53 <jim.rapenne@gmail.com>
2025-10-21 07:55:40 +0000

    6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -16,6 +16,6 @@ int main()
 	printf("Size of a long int: %d byte(s)\n", (int)sizeof(l));
 	printf("Size of a long long int: %d byte(s)\n", (int)sizeof(ll));
 	printf("Size of a float: %d byte(s)\n", (int)sizeof(f));
-
+}
 	return 0;
 }
