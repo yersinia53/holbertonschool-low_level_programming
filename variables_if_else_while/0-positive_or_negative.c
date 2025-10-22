dc3896612ea4b32bf8de34ccadf85b6d371d2371
yersinia53 <jim.rapenne@gmail.com>
2025-10-22 07:56:07 +0000

    0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - entry point, prints a random number and whether it is positive, negative, or zero
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 	int n;
